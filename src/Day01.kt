5ad64ba1cca30857b838ee57d6d3b1365507ebb5
Azmi Nabeel <70376685+azmi-nabeel@users.noreply.github.com>
2024-12-11 00:39:32 +0530

    Day-1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,54 @@
+import java.util.ArrayList
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val a= ArrayList<Int>()
+        val b= ArrayList<Int>()
+        var lineNo=0
+        while (lineNo<input.size) {
+            val line= input[lineNo].split("   ").map { it.toInt() }
+            a.add(line[0])
+            b.add(line[1])
+            lineNo++
+        }
+        a.sort()
+        b.sort()
+        val sz=a.size
+        var ans=0
+        for (i in 0..<sz){
+            ans+= abs(a[i]-b[i])
+        }
+        return ans
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val a= ArrayList<Int>()
+        val b= mutableMapOf<Int,Int>()
+        var lineNo=0
+        while (lineNo<input.size) {
+            val line= input[lineNo].split("   ").map { it.toInt() }
+            a.add(line[0])
+            if(b.containsKey(line[1])) {
+                b[line[1]]=b[line[1]]!!.inc()
+            }
+            else {
+                b[line[1]] = 1
+            }
+            lineNo++
+        }
+        var ans=0
+        val sz=a.size
+        for (i in 0..<sz){
+            if(b.containsKey(a[i])){
+                ans+=(a[i]* b[a[i]]!!)
+            }
+        }
+        return ans
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    // Read the input from the `src/input.txt` file.
+    val input = readInput("input")
     part1(input).println()
     part2(input).println()
 }
